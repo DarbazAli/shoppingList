63811538185072bc72aacd3ebd824aa761f8085c
Darbaz Ali <darbaz.me@gmail.com>
2022-02-04 17:32:19 +0300

    using image

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
-import React, {Component} from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import React from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.heading}> Hello World! </Text>
-      </View>
-    );
-  }
-}
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>Hello World</Text>
+      <Image
+        source={{uri: 'https://randomuser.me/api/portraits/men/9.jpg'}}
+        style={styles.img}
+      />
+    </View>
+  );
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
@@ -20,5 +24,10 @@ const styles = StyleSheet.create({
   heading: {
     fontSize: 20,
     fontWeight: '500',
+  },
+  img: {
+    width: 100,
+    height: 100,
+    borderRadius: 100 / 2,
   },
 });
